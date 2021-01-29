ee4f6c897816f204dace0cf16a022566ada9a530
rStar <starlove124760@gmail.com>
2021-01-30 03:54:16 +0800

    feat: add gamemanager

---
+++ Assets/Scripts/Main/GameManager.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace Main
+{
+    public class GameManager : MonoBehaviour
+    {
+        private void Start()
+        {
+            Application.targetFrameRate = 60;
+        }
+    }
+}
