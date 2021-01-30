856d35e06880f721a6813b2d57e34d62710a3adf
rStar <starlove124760@gmail.com>
2021-01-30 16:05:21 +0800

    feat: update scene

---
+++ Assets/Resource/Scripts/Item/OnItemDestory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnItemDestory : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> activateObjects = new List<GameObject>();
+
+    // Start is called before the first frame update
+    private void OnDestroy()
+    {
+        foreach (var activateObject in activateObjects)
+        {
+           activateObject.SetActive(true);
+        }
+    }
+}
