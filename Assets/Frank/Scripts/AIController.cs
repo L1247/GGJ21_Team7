b0f7b016f9b1fb8526138a3d963dd74ae97ed203
MSI\Frank <tim4224171@gmail.com>
2021-01-30 09:28:42 +0800

    AI初步雛形，跟簡易玩家方便測試

---
+++ Assets/Frank/Scripts/AIController.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+    #region 宣告
+    [Header("移動速度")]
+    [SerializeField] float moveSpeed = 2f;
+    [Header("偵測距離")]
+    [SerializeField] float distance = 5f;
+
+    [Header("巡邏範圍")]
+    [SerializeField] Transform rightWall;
+    [SerializeField] Transform leftWall;
+    [Header("停留時間")]
+    [SerializeField] float startStayTime = .1f;
+
+    private GameObject player;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+    private bool IsMoveRight;
+    #endregion
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (IsInRange())
+        {
+            FollowBehaviour();
+        }
+        else
+        {
+            PatrolBehaviour();
+        }
+    }
+
+    private void FollowBehaviour()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+
+        if (IsPlayerOnRight())
+        {
+            sprite.flipX = false;
+        }
+        else
+        {
+            sprite.flipX = true;
+        }
+    }
+
+    private void PatrolBehaviour()
+    {
+        if (IsMoveRight)
+        {
+            transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+            sprite.flipX = false;
+        }
+        else
+        {
+            transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
+            sprite.flipX = true;
+        }
+    }
+
+    private bool IsInRange()
+    {
+        return Vector2.Distance(transform.position, player.transform.position) < distance;
+    }
+
+    private bool IsPlayerOnRight()
+    {
+        return player.transform.position.x - transform.position.x > 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Wall")
+        {
+            IsMoveRight = !IsMoveRight;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, distance);
+    }
+}
---
+++ Assets/Frank/Scripts/Frank_PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+public class Frank_PlayerController : MonoBehaviour
+{
+    [Tooltip("移動速度")]
+    [SerializeField] float moveSpeed = .1f;
+    [Tooltip("跳躍力道")]
+    [SerializeField] float jumpForce = 6f;
+
+    Rigidbody2D rig2D;
+
+    private void Start()
+    {
+        rig2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        InputKey();
+    }
+
+    private void InputKey()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * moveSpeed;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * moveSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rig2D.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
