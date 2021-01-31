a0417cafaa2945a920498ff2d85b4adf6e414e0c
rStar <starlove124760@gmail.com>
2021-01-31 13:56:03 +0800

    feat: add heart effect

--- Assets/Scripts/UIHeartEffect.cs
+++ Assets/Scripts/UIHeartEffect.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
@@ -15,14 +16,20 @@ public class UIHeartEffect : MonoBehaviour
     {
         foreach (var heartImage in _heartImages)
         {
-            heartImage.sprite.ObserveEveryValueChanged(s => s)
-                      .Subscribe(sprite => OnSpriteChanged(heartImage , sprite));
+            var animateHeart = Instantiate(heartImage.gameObject, heartImage.transform.parent);
+            heartImage.ObserveEveryValueChanged(s => s.sprite)
+                      .Where(sprite =>sprite.name == "血量_線稿")
+                      .Subscribe(sprite => OnSpriteChanged(heartImage,animateHeart));
         }
     }
 
-    private void OnSpriteChanged(Image heartImage , Sprite sprite)
+    private void OnSpriteChanged(Image heartImage , GameObject animateHeart)
     {
-        Debug.Log($"{heartImage} , {sprite}");
+        Debug.Log($"{heartImage.sprite}");
+        var image = animateHeart.GetComponent<Image>();
+        animateHeart.transform.DOMoveY(1.5f , 5).SetEase(Ease.OutQuad);
+        animateHeart.transform.DORotate(new Vector3(0,0,-45) , 0.8f).SetEase(Ease.InQuad);
+        image.DOFade(0 , 1.5f);
     }
 
     // Update is called once per frame
